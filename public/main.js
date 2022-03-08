4db4ff9d1dd3fe361e5ed679624e2e1128ca73fd
ceblakely <cblakelydev@gmail.com>
2022-03-07 23:24:31 -0500

    Start slideshow & modal

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
-const shopLi = document.getElementById("shopLi");
 const menuIcon = document.getElementById("menuIcon");
 const mobileMenu = document.getElementById("mobileMenu");
 
+const modal = document.getElementById("modalPopup");
 function openNav(e) {
   mobileMenu.classList.toggle("menuOpen");
   e.preventDefault();
@@ -10,3 +10,12 @@ function closeNav(e) {
   mobileMenu.classList.toggle("menuClosed");
   e.preventDefault();
 }
+function openModal(e) {
+  modal.style.display = "block";
+  e.preventDefault();
+}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
